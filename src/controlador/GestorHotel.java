f3328a858909b4d53a687e169cbc0bab203080fe
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-02-26 13:51:09 +0100

    tt

--- src/controlador/GestorHotel.java
+++ src/controlador/GestorHotel.java
@@ -1,5 +1,30 @@
 package controlador;
 
+import java.util.Scanner;
+
+import vista.Menu;
+
 public class GestorHotel {
+    public static void main(String[] args) {
+        int opcion = 0;
+        Scanner scan = new Scanner(System.in);
+        
+        do {
+        	Menu.menuPrincimpal();
+			opcion = Integer.parseInt(scan.nextLine());
+			switch (opcion) {
+			case Menu.DAR_DE_ALTA:
+				dardeAltaClientye();
+				break;
+			default:
+				break;
+			}
+		} while (opcion!=Menu.SALIR);
+    }
+
+	private static void dardeAltaClientye() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -1,41 +1,31 @@
 package vista;
 
 public class Menu {
-	private final static int GESTOR_HOTEL = 3;
-	private final static int SALIR = 0;
 	
-    private static final int DAR_DE_ALTA = 1;
-    private static final int MODIFICAR_CLIENTE = 2;
-    private static final int BORRAR_CLIENTE = 3;
+	public final static int SALIR = 0;
+	public static final int DAR_DE_ALTA = 1;
+	public static final int MODIFICAR_CLIENTE = 2;
+	public static final int BORRAR_CLIENTE = 3;
     
-    private static final int DAR_DE_ALTA_RESERVA = 1;
-    private static final int DAR_DE_BAJA_RESERVA = 2;
+	public static final int DAR_DE_ALTA_RESERVA = 4;
+    public static final int DAR_DE_BAJA_RESERVA = 5;
+    public static final int DAR_DE_ALTA_HOTEL = 6;
 	
 	public Menu() {
 		
 	}
 	
-	private static void mostrarMenuPrincimpal() {
-	    System.out.println("MENU");
-	    System.out.println(GESTOR_HOTEL + ". Gestor Hotel");
-	    System.out.println(SALIR + ". Salir");
-	    System.out.print("Seleccione una opción: ");
-	}
-
-
-	public static void menuReserva() {
-        System.out.println("MENU");
-        System.out.println(DAR_DE_ALTA_RESERVA + ". Dar de alta reserva");
-        System.out.println(DAR_DE_BAJA_RESERVA + ". Dar de baja reserva");
-        System.out.println(SALIR + ". Salir");
-        System.out.print("Seleccione una opción: ");
-	}
-	public static void menuClientes() {
+	public static void menuPrincimpal() {
         System.out.println("MENU");
         System.out.println(DAR_DE_ALTA + ". Dar de alta cliente");
         System.out.println(MODIFICAR_CLIENTE + ". Modificar cliente");
         System.out.println(BORRAR_CLIENTE + ". Borrar cliente");
+
+        System.out.println(DAR_DE_ALTA_RESERVA + ". Dar de alta reserva");
+        System.out.println(DAR_DE_BAJA_RESERVA + ". Dar de baja reserva");
+        System.out.println(DAR_DE_ALTA_HOTEL + ". Dar de alta Hotel");
         System.out.println(SALIR + ". Salir");
         System.out.print("Seleccione una opción: ");
+		
 	}
 }
