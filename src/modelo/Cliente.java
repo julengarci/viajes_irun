25dab80765fc5c5ab66fd40a7b9fdd1c52a2641b
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-02-26 13:35:14 +0100

    mi primer commit

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,5 +1,55 @@
 package modelo;
 
 public class Cliente {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	
+	public Reserva getReserva() {
+		return reserva;
+	}
+	public void setReserva(Reserva reserva) {
+		this.reserva = reserva;
+	}
+	private String localidad;
+	Reserva reserva;
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cliente [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion
+				+ ", localidad=" + localidad + ", reserva=" + reserva + "]";
+	}
 
+	
 }
--- src/modelo/Hotel.java
+++ src/modelo/Hotel.java
@@ -1,5 +1,59 @@
 package modelo;
 
 public class Hotel {
-
+	private int id;
+	private String cif;
+	private String nombre;
+	private String gerente;
+	private int entrella;
+	private String compania;
+	Habitacion habitacion;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCif() {
+		return cif;
+	}
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getGerente() {
+		return gerente;
+	}
+	public void setGerente(String gerente) {
+		this.gerente = gerente;
+	}
+	public int getEntrella() {
+		return entrella;
+	}
+	public void setEntrella(int entrella) {
+		this.entrella = entrella;
+	}
+	public String getCompania() {
+		return compania;
+	}
+	public void setCompania(String compania) {
+		this.compania = compania;
+	}
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+	@Override
+	public String toString() {
+		return "Hotel [id=" + id + ", cif=" + cif + ", nombre=" + nombre + ", gerente=" + gerente + ", entrella="
+				+ entrella + ", compania=" + compania + ", habitacion=" + habitacion + "]";
+	}
+	
 }
